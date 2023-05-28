1b1bb8d832f68f472c709bd56618101816ffd552
SiyabongaM225 <103603030+SiyabongaM225@users.noreply.github.com>
2023-05-28 15:55:59 +0200

    Update main.dart
    In this updated main.dart file, I added a Dark Charcoal Theme.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
-
+// Press ctrl + . and ignore the error or warning about outdated widgets for the file
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final ThemeData charcoalTheme = ThemeData(
+    brightness: Brightness.dark,
+    primaryColor: Colors.grey[900],
+    accentColor: Colors.grey[500],
+    scaffoldBackgroundColor: Colors.grey[900],
+    textTheme: TextTheme(
+      bodyText1: TextStyle(color: Colors.grey[500]),
+      bodyText2: TextStyle(color: Colors.grey[500]),
+    ),
+    appBarTheme: AppBarTheme(
+      color: Colors.grey[900],
+      iconTheme: IconThemeData(color: Colors.grey[500]),
+      textTheme: TextTheme(
+        headline6: TextStyle(
+          color: Colors.grey[500],
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: 'Charcoal Theme',
+      theme: charcoalTheme,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Charcoal Theme'),
+        ),
+        body: Center(
+          child: Text(
+            'Hello, World!',
+            style: Theme.of(context).textTheme.headline6,
+          ),
+        ),
+      ),
+    );
   }
 }
