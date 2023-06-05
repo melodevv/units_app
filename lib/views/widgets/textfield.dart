ef4a5aafe0fb931ff70eec33b9e5431252aaeb07
Siyabonga Tshabalala <103603030+SiyabongaM225@users.noreply.github.com>
2023-06-06 01:17:18 +0200

    Update textfield.dart

--- lib/views/widgets/textfield.dart
+++ lib/views/widgets/textfield.dart
@@ -9,21 +9,24 @@ class InputField extends StatelessWidget {
   final TextInputAction? textInputAction;
   final bool autoFocus;
   final bool obscureText;
-  const InputField(
-      {this.labelText,
-      this.onChanged,
-      this.onSubmitted,
-      this.errorText,
-      this.keyboardType,
-      this.textInputAction,
-      this.autoFocus = false,
-      this.obscureText = false,
-      Key? key})
-      : super(key: key);
+  final TextEditingController controller;
+  const InputField({
+    this.labelText,
+    this.onChanged,
+    this.onSubmitted,
+    this.errorText,
+    this.keyboardType,
+    this.textInputAction,
+    this.autoFocus = false,
+    this.obscureText = false,
+    required this.controller,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      controller: controller,
       autofocus: autoFocus,
       onChanged: onChanged,
       onSubmitted: onSubmitted,
